d028c6e9b446a3fc7034ebafb072b80930e31ba2
aleemchishti <chishti_aleem@yahoo.com>
2022-12-05 09:47:57 -0500

    added devise for signup,added user views,bootstrap thumbnails for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
  root "admin/home#index" 
 
+ devise_for :users
+ resources :users
+
  namespace :admin do
  resources :products, :warehouse, :home
  resources :orders, :customers, :categories
